50b6de009f25c76dbffe77b8b1a5f32d1825e9d1
BeainaT <tania.df@hotmail.it>
2022-05-04 12:25:56 +0200

    added number + print sum

--- snack-3/main.js
+++ snack-3/main.js
@@ -1,6 +1,8 @@
 //Chiedere per 10 volte all'utente di inserire un numero
+let sum = 0;
 for( i = 0; i < 10; i++ ) {
-    let number = prompt("inserisci un numero");
+    let number = Number(prompt("inserisci un numero"));
+    sum = sum + number;
 }
 //Stampare la somma di tutti i numeri inseriti dall'utente
-console.log(number * 10);
+console.log(sum);
