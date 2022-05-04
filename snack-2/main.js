93b47ea8dbffa4b93c9ee8646e4086a00a74fd12
BeainaT <tania.df@hotmail.it>
2022-05-04 11:52:05 +0200

    added else if

--- snack-2/main.js
+++ snack-2/main.js
@@ -4,6 +4,8 @@ const secondWord = prompt("Inserisci una nuova parola");
 //Stampare prima la parola più corta e successivamente quella più lunga
 if (firstWord.length > secondWord.length) {
     console.log(secondWord, firstWord);
+} else if (firstWord === secondWord) { 
+    console.log("le parole hanno la stessa lunghezza");
 } else {
     console.log(firstWord, secondWord);
 }
