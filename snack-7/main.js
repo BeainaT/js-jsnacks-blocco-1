1b9c18e4e3d3ed6cb458193f1f043df3f02ded80
BeainaT <tania.df@hotmail.it>
2022-05-04 21:57:39 +0200

    added random list

--- snack-7/main.js
+++ snack-7/main.js
@@ -2,9 +2,11 @@
 let nameList = ["Arturo", "Luca", "Elisa", "Armando"]
 //Generare lista di cognomi
 let lastNameList = ["Rossi", "Bianchi", "Verdi", "Fumagalli"]
-//Generare una lista di nomi+cognomi
+//Generare una lista randomica di nomi+cognomi
 let fullNameList = [];
 for( i=0; i < 4; i++ ) {
-    fullNameList.push(`${nameList[i]} ${lastNameList[i]}`);
+    nameRandom = Math.floor(Math.random() * 4);
+    lastNameRandom = Math.floor(Math.random() * 4);
+    fullNameList.push(`${nameList[nameRandom]} ${lastNameList[lastNameRandom]}`);
 }
-console.log(fullNameList)
+console.log(fullNameList);
