c08f5a8a5e54a1d1cc978c8dbc2453d132ca2228
BeainaT <tania.df@hotmail.it>
2022-05-04 11:35:17 +0200

    added code

---
+++ snack-1/main.js
@@ -0,0 +1,9 @@
+//Chiedere all'utente di inserire due numeri
+const firstNumber = Number(prompt("Inserisci un numero"));
+const secondNumber = Number(prompt("Inserisci un altro numero"));
+//Stampare il maggiore
+if (firstNumber > secondNumber) {
+    console.log(firstNumber);
+} else {
+    console.log(secondNumber);
+}
