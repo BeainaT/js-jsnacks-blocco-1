ceb510aa83aaba23483be869b3095eb35a311907
BeainaT <tania.df@hotmail.it>
2022-05-04 23:12:58 +0200

    added js code

--- snack-9/main.js
+++ snack-9/main.js
@@ -1,2 +1,12 @@
 // Creare due array con numero diverso di elementi
+const shortArr = ["blue", "red", "green"];
+const longArr = ["pink", "yellow", "violet", "black", "gray", "orange"];
 // Aggiungere elementi all'array con lunghezza minore finchè non saranno di pari lunghezza
+// Ciclo per la lunghezza dell'array con più elementi
+for( i = 0; i <= longArr.length; i++) {
+    //SE lunghezza di un array < lunghezza altro array, aggiungo elementi
+    if (shortArr.length < longArr.length) {
+    shortArr.push("jolly color");
+    }
+}
+console.log(shortArr, longArr);
