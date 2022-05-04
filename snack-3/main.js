db6fccb78b8e993ef5476123eca3f79f70f36df4
BeainaT <tania.df@hotmail.it>
2022-05-04 12:10:29 +0200

    added for cicle

---
+++ snack-3/main.js
@@ -0,0 +1,6 @@
+//Chiedere per 10 volte all'utente di inserire un numero
+for( i = 0; i < 10; i++ ) {
+    let number = prompt("inserisci un numero");
+}
+//Stampare la somma di tutti i numeri inseriti dall'utente
+console.log(number * 10);
