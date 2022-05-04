3933f56d2049ca5a14e53895d34562e4ca7af057
BeainaT <tania.df@hotmail.it>
2022-05-04 17:26:07 +0200

    fixed code (- random)

--- snack-7/main.js
+++ snack-7/main.js
@@ -5,7 +5,6 @@ let lastNameList = ["Rossi", "Bianchi", "Verdi", "Fumagalli"]
 //Generare una lista di nomi+cognomi
 let fullNameList = [];
 for( i=0; i < 4; i++ ) {
-    fullNameList.push(nameList[i]);
-    fullNameList.push(lastNameList[i]);
+    fullNameList.push(`${nameList[i]} ${lastNameList[i]}`);
 }
 console.log(fullNameList)
